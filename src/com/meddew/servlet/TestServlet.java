02d60a6bd92be275720744d9762cca6b46cef292
Med <mehdi.gacem@versusmind.eu>
2019-11-02 18:19:44 +0100

    basic servlet creation

---
+++ src/com/meddew/servlet/TestServlet.java
@@ -0,0 +1,32 @@
+package com.meddew.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding( "UTF-8" );
+		PrintWriter out = resp.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"utf-8\" />");
+		out.println("<title>Test</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>Ceci est une page générée depuis une servlet.</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+ 
+}
